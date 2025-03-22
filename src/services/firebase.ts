91642d18d666d3fb911880fd24800c217a41bed5
Gabrielle Earnshaw <g_earnshaw@hotmail.com>
2025-03-22 19:51:24 +0000

    Added storage and updated the firebase service

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,15 +3,19 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  // TODO: Add your Firebase configuration here
-  // apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  // authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  // projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
 export const db = getFirestore(app);
+
+// Initialize Auth
 export const auth = getAuth(app);
