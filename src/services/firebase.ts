9191e8a32aa2a2d83bb82e4aebb1c69408716281
Gabrielle Earnshaw <g_earnshaw@hotmail.com>
2025-04-03 18:09:44 +0100

    Suppressed firebase warnings

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,7 +2,13 @@
 import RNAuth from '@react-native-firebase/auth';
 import RNFirestore from '@react-native-firebase/firestore';
 
-// export const app = initializeApp(firebaseConfig);
-// export const db = getFirestore(app);
+// Suppress the deprecation warning for Firebase
+declare global {
+  var RNFB_SILENCE_MODULAR_DEPRECATION_WARNINGS: boolean;
+}
+
+globalThis.RNFB_SILENCE_MODULAR_DEPRECATION_WARNINGS = true;
+// END OF Suppress the deprecation warning for Firebase
+
 export const auth = RNAuth();
 export const db = RNFirestore();
